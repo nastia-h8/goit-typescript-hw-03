e14c23815c3b373f608e74de846b16dc9a53ed91
Anastasiia Haliara <an.haliara@gmail.com>
2023-10-24 18:26:35 +0300

    accomplish tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+    this.door = false;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Come in!");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open");
+    } else {
+      console.log("Wrong key");
+    }
+  }
+}
+
 const key = new Key();
+key.getSignature();
+
+const person = new Person(key);
+person.getKey();
 
 const house = new MyHouse(key);
-const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
